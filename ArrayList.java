976b56f4dcc89ab16b0154849990c70d225a4096
Abhi <abhishekac@maveric-systems.com>
2022-04-07 16:50:20 +0530

    new file added

---
+++ ArrayList.java
@@ -0,0 +1,19 @@
+
+
+import java.util.ArrayList;
+
+public class Arraylist
+{
+	public static void main(String args[])
+	{
+		ArrayList al=new ArrayList();
+	
+		al.add(100);
+		al.add(200);
+		al.add(123);
+		al.add(234);
+		
+		System.out.println("Array List Are"+al);
+	}
+}
+
